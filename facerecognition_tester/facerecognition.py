0253b23368a3953a51201f5abb97917f1cba46b9
Bryn Dole <bryn.dole@gmail.com>
2017-01-02 09:52:35 -0800

    translate messages from German to English. add support for non-windowing environment. fix off by one error.

--- facerecognition_tester/facerecognition.py
+++ facerecognition_tester/facerecognition.py
@@ -17,6 +17,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 import cv2  # OpenCV Library
 import lib.face as face
 import lib.config as config
+import time
+import os
 
 # Load training data into model
 print('Loading training data...')
@@ -36,9 +38,11 @@ print('Training data loaded!')
 
 camera = config.get_camera()
 
+time.sleep(1)   # give the camera a second to warm up
 while True:
     # camera video feed
     frame = camera.read()
+
     image = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
     faces = face.detect_face(image)
@@ -61,19 +65,23 @@ while True:
                 if h > 190 and confidence < config.POSITIVE_THRESHOLD:
                     cv2.putText(frame, config.users[label - 1], (x - 3, y - 8), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 1)
                     cv2.putText(frame, str(confidence), (x - 2, y + h + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                    print('User:' + config.users[label - 1])    
                 # If person is further away from the camera but POSITIVE_THRESHOLD is still under 40 assume it is the person
-                elif h < 190 and confidence < config.POSITIVE_THRESHOLD:
+                elif h <= 190 and confidence < config.POSITIVE_THRESHOLD:
                     cv2.putText(frame, config.users[label - 1], (x - 3, y - 8), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 1)
                     cv2.putText(frame, str(confidence), (x - 2, y + h + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                    print('User:' + config.users[label - 1])
                 # If person is further away from the camera be a bit more generous with the POSITIVE_THRESHOLD and add a not sure statement
                 elif h < 190:
-                    cv2.putText(frame, "Vermute: " + config.users[label - 1], (x - 3, y - 8), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
+                    cv2.putText(frame, "Guess: " + config.users[label - 1], (x - 3, y - 8), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
                     cv2.putText(frame, str(confidence), (x - 2, y + h + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                    print('Guess:' + config.users[label - 1])
                 else:
-                    cv2.putText(frame, "Unbekannt", (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 1)
-
+                    cv2.putText(frame, "Unknown", (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 1)
+                    print('Unknown face')
             else:
-                cv2.putText(frame, "Unbekannt", (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 1)
+                cv2.putText(frame, "Unknown", (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 1)
+                print('Unknown face')
 
             # If person is close enough
             if h > 250:
@@ -81,13 +89,17 @@ while True:
                 for i in range(0, len(eyes)):
                     x, y, w, h = eyes[i]
                     cv2.rectangle(frame, (x + x_face, y + y_face - 30), (x + x_face + w + 10, y + y_face + h - 40), (94, 255, 0))
-                    cv2.putText(frame, "Auge " + str(i), (x + x_face, y + y_face - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                    cv2.putText(frame, "Eye " + str(i), (x + x_face, y + y_face - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
+    if ('DISPLAY' in os.environ):
         # Display Image
-    cv2.imshow('Facial recognition', frame)
-
-    # exit by pressing q
-    if cv2.waitKey(1) == ord('q'):
+        cv2.imshow('Facial recognition', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    else:
+        print('writing face.jpg image')
+        cv2.imwrite('face.jpg',frame)
+        camera.stop()
         break
 
 # Release camera and close windows
